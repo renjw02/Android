72e2a231ef276e59916fbcad1b78285fe3383dcf
rjw <rjw20020718@163.com>
2023-05-03 21:04:07 +0800

    modified resetpw

--- backend/app/services/user.py
+++ backend/app/services/user.py
@@ -88,7 +88,7 @@ class UserService():
             return "errors", False
 
     
-    def reset_pw(self, username, mobile, password):
+    def reset_pw(self, username, password):
         try:
             pw = encrypt_password(password)
             db.session.query(User).filter(User.username == username).update({
